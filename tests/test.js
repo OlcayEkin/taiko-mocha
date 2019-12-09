2d8245c58752c007bc7a5d32739662481fab1b71
OlcayEkin <olcayekinn@gmail.com>
2019-12-09 22:06:56 +0300

    First commit

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,5 @@
 "use strict";
-const { openBrowser, write, closeBrowser, goto, press, text, focus, inputField, toRightOf } = require('taiko');
+const { openBrowser, write, closeBrowser, goto, press, text, focus, inputField, toRightOf, intercept, click, listItem, below, deleteCookies } = require('taiko');
 const assert = require("assert");
 const headless = false;
 
@@ -9,20 +9,21 @@ describe('Getting Started with Mocha and Taiko', () => {
         await openBrowser({ headless: headless });
     });
 
-    describe('Search Taiko Repository', () => {
+    describe('Trendyol check list item', () => {
 
-        it('Goto getgauge github page', async () => {
-            await goto('https://github.com/getgauge');
+        it('Go to trendyol.com', async () => {
+            await intercept("https://www.trendyol.com/api/home/banners?systemName=HomeIndex&position=right&cssClass=bannerItem%20homepage-banner-item&isMillaboutique=False");
+            await goto("https://www.trendyol.com/");
         });
 
-        it('Search for "Taiko"', async () => {
-            await focus(inputField(toRightOf('Pricing')))
-            await write('Taiko');
-            await press('Enter');
+        it('Search for "Erkek Saat"', async () => {
+            await click("Erkek");
+            await write("Erkek Saat", into(textBox({placeholder: "Aradığınız ürün veya markayı yazınız"})));
+            await press("Enter");
         });
 
-        it('Page contains "getgauge/taiko"', async () => {
-            assert.ok(await text('getgauge/taiko').exists());
+        it('Check list items', async () => {
+            await listItem(below('Erkek Saat')).exists();
         });
 
     });
